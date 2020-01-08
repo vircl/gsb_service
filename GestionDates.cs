0332075da5b9106bb429e1abbca0da9255d6fc92
Virginie CLAUDE <dev@virginie-claude.fr>
2020-01-08 18:42:30 +0100

    Ajout de la classe date (méthodes vides) + initialisation du framework de tests

---
+++ GestionDates.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppliGestionCloture
+{
+    public class GestionDates
+    {
+        /// <summary>
+        /// Retourne le n° du jour de la date passée en paramètres
+        /// </summary>
+        /// <param name="uneDate">DateTime : Date à tester</param>
+        /// <returns>String : n° du jour sur deux chiffres </returns>
+        public string getNumeroJour(DateTime uneDate)
+        {
+
+        }
+        /// <summary>
+        /// Retourne le n° du mois précédant la date courante
+        /// </summary>
+        /// <returns>String n° du mois sur 2 chiffres</returns>
+        public string getMoisPrecedent()
+        {
+
+        }
+        /// <summary>
+        /// Retourne le n° du mois précédant la date passée en paramètres
+        /// </summary>
+        /// <param name="uneDate">Date à tester</param>
+        /// <returns>String n° du mois sur 2 chiffres</returns>
+        public string getMoisPrecedent(DateTime uneDate)
+        {
+
+        }
+        /// <summary>
+        /// Retourne le n° du mois suivant la date courante 
+        /// </summary>
+        /// <returns>String : le n° du mois sur deux chiffres</returns>
+        public string getMoisSuivant()
+        {
+
+        }
+        /// <summary>
+        /// Retourne le n° du mois suivant la date passée en paramètres
+        /// </summary>
+        /// <param name="uneDate">Date à tester</param>
+        /// <returns>String : n° du mois sur 2 chiffres</returns>
+        public string getMoisSuivant(DateTime uneDate)
+        {
+
+        }
+        /// <summary>
+        /// Teste si le n° de jour de la date actuelle se trouve entre les bornes passées en paramètre
+        /// </summary>
+        /// <param name="min">string : Borne min</param>
+        /// <param name="max">string : Borne max</param>
+        /// <returns>Boolean : True si la date est située entre les bornes</returns>
+        public bool entre(string min, string max)
+        {
+
+        }
+        /// <summary>
+        /// Teste si le n° de jour de la date passée en paramètres se trouve entre les bornes min et max
+        /// </summary>
+        /// <param name="min">string : Borne min</param>
+        /// <param name="max">string : Borne max</param>
+        /// <param name="uneDate">DateTime : La date à tester</param>
+        /// <returns>Boolean : True si la date est située entre les bornes</returns>
+        public bool entre(string min, string max, DateTime uneDate)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,24 @@ namespace AppliGestionCloture
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            string chaineConnexion = "server=localhost;port=3308;user id=root;database=gsb_frais;SslMode=none";
+            testSQL(chaineConnexion);
+        }
+        static void testSQL(string chaineConnexion)
+        {
+            try
+            {
+                ConnexionSql sql = new ConnexionSql(chaineConnexion);
+                Console.Write("Init OK");
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+            Console.ReadLine();
         }
     }
 }
